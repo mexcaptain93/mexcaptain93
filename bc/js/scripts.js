ecdaf9b02155d1d580bbe4664d5b8a9011e444cc
mexcaptain <mikhail@hashboard.ru>
2017-08-23 11:43:56 +0300

    Timepicker elem

--- bc/js/scripts.js
+++ bc/js/scripts.js
@@ -1,10 +1,28 @@
 $(function() {
 	datePickerInit();
+	timePickerInit();
 });
 
 function datePickerInit() {
 	$('.datepicker').datepicker({
 		format: "dd.mm.yyyy",
 		language: "ru"
+	});
+}
+
+function timePickerInit() {
+	$('.timepicker').timepicker({
+		showMeridian: false,
+		minuteStep: 5,
+		showInputs: true,
+	}).on('changeTime.timepicker', function(e) {
+		var h  = e.time.hours;
+		var m  = e.time.minutes;
+		if (h*60+m  < 480) {
+			$(this).timepicker('setTime', '08:00');
+		}
+		if (h*60+m > 1200) {
+			$(this).timepicker('setTime', '20:00');
+		}
 	});
 }
