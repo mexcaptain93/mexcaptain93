dcea44353db8042678c833aabc777696a3bf1def
Mikhail <mexcaptain@gmail.com>
2017-11-14 20:53:27 +0300

    stars

---
+++ stars/js/scripts.js
@@ -0,0 +1,41 @@
+$(function(){
+
+    let checked = localStorage.getItem('checked');
+
+    if (checked != null) {
+        starsAddColor(checked);
+    }
+
+    $('.js-star').on('click', function(e) {
+        e.preventDefault();
+        let value = $(this).index();
+        starsAddColor(value);
+        checked = value;
+    });
+
+    $('.js-star').on('mouseover', function(e) {
+        e.preventDefault();
+        let value = $(this).index();
+        starsAddColor(value);
+    });
+
+    $('.stars__list').on('mouseout', function (e) {
+        e.preventDefault();
+        if (checked == null) {
+            $('.js-star').removeClass('stars__item_checked');
+        }
+    });
+
+    $('.js-save').on('click', function (e) {
+        e.preventDefault();
+        localStorage.setItem('checked', checked)
+    });
+
+    function starsAddColor(val) {
+
+        $('.js-star').removeClass('stars__item_checked');
+        for (let i=0; i<=val; i++) {
+            $('.js-star').eq(i).addClass('stars__item_checked');
+        }
+    }
+});
