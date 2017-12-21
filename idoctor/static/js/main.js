b517611d667c7e8ae2c019364bf4a7e80f2fd2a8
Mikhail <mexcaptain@gmail.com>
2017-12-21 07:35:01 +0300

    Work on index page

--- idoctor/static/js/main.js
+++ idoctor/static/js/main.js
@@ -1,3 +1,56 @@
 $(document).ready(function () {
-    svg4everybody({});
+    toggleHeaderSearch();
+    togglers();
 });
+
+
+function toggleHeaderSearch() {
+    $('.js-show-header-search-bar').on('click', function (e) {
+        e.preventDefault();
+        $('.js-header-search-bar').addClass('header__search_visible');
+        console.log('dich')
+    });
+
+    $(document).click(function (event) {
+        if ((!$(event.target).closest('.js-header-search-bar').length) && !$(event.target).closest('.js-show-header-search-bar').length ) {
+           if ($('.js-header-search-bar').hasClass('header__search_visible')) {
+               $('.js-header-search-bar').removeClass('header__search_visible');
+           }
+        }
+    });
+}
+
+function togglers() {
+    var selectedType = '';
+    var typeToggler = $('.js-toggler-type');
+    var typesQuantity = typeToggler.find('[data-value]').length;
+    refreshBack(typeToggler, typesQuantity);
+
+    typeToggler.find('[data-value]').on('click',function (e) {
+        e.preventDefault();
+        selectedType = $(this).attr('data-value');
+        typeToggler.attr('data-value', selectedType);
+        refreshBack(typeToggler, typesQuantity);
+    });
+
+
+    var selectedModel = '';
+    var modelToggler = $('.js-toggler-model');
+    var modelsQuantity = modelToggler.find('[data-value]').length;
+
+    refreshBack(modelToggler, modelsQuantity);
+
+    modelToggler.find('[data-value]').on('click',function (e) {
+        e.preventDefault();
+        selectedModel = $(this).attr('data-value');
+        modelToggler.attr('data-value', selectedModel);
+        refreshBack(modelToggler, modelsQuantity);
+    });
+
+
+    function refreshBack(el, q) {
+        var index = el.find("[data-value='" + el.attr('data-value') + "']").index();
+        el.find('.toggler__back').css({'left':el.width() / q * index});
+        console.log(index)
+    }
+}
