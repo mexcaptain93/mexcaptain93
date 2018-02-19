8572d64a5624827264b68d7bef3f6cf738eefd3e
Mikhail <mexcaptain@gmail.com>
2018-02-19 16:41:49 +0300

    1902

--- druzhba/static/js/main.js
+++ druzhba/static/js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    svg4everybody({});
+    popup();
 });
+
+function popup() {
+    $('.js-want').on('click', function (e) {
+        e.preventDefault();
+        $('.js-popup-want').show();
+        $('body').addClass('stop-scrolling')
+
+    });
+
+
+
+    $($('.js-popup-want')).click(function(event) {
+        if($('.js-popup-want').is(":visible")) {
+
+            if (!$(event.target).closest('.js-popup-want .popup__content').length) {
+                $('.js-popup-want').hide();
+                $('body').removeClass('stop-scrolling')
+
+            }
+        }
+    });
+}
