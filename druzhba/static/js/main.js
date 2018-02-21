33bc2eb7a7e4f9334f21e3aadc55c25a99f477a1
Mikhail <mexcaptain@gmail.com>
2018-02-21 17:32:39 +0300

    2102

--- druzhba/static/js/main.js
+++ druzhba/static/js/main.js
@@ -24,16 +24,19 @@ function popup() {
     });
 
 
-    $('.js-send-mail').on('click', function () {
+    $('.js-send-mail').on('click', function (e) {
+        e.preventDefault();
         $.ajax({
             type: "POST",
             url: "mail.php",
             data: "name="+ $('.js-name').val() + "&phone="+ $('.js-phone').val(),
             success: function(msg){
-                $('.js-popup-want .popup__list').remove();
-                $('.js-popup-want .like-h3').text(msg);
+                $('.js-popup-want .popup__list').hide();
+                $('.js-popup-want').append('<span class="like-h3 popup__answer">' + msg + '</span>');
                 setTimeout(function() {
                     $('.js-popup-want').hide();
+                    $('.js-popup-want .popup__list').show();
+                    $('.js-popup-want .popup__answer').remove();
                     $('body').removeClass('stop-scrolling')
                 }, 3000);
 
