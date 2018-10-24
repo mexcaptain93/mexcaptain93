0386ecd469032dbaa5107e8e35320622685ace82
Mikhail <mexcaptain@gmail.com>
2018-10-24 19:52:19 +0300

    form fix + mask

--- bit/static/js/main.js
+++ bit/static/js/main.js
@@ -1,6 +1,7 @@
 $(function () {
     slider();
     phoneMask();
+    scroll();
 });
 
 function slider() {
@@ -27,4 +28,43 @@ function phoneMask() {
        mask: '+{7}(000) 000-00-00'
    });
    mask.rawValue = '999-12-12-123';
+}
+
+function scroll() {
+    var menu_selector = ".js-menu";
+
+    function onScroll() {
+        var scroll_top = $(document).scrollTop();
+
+        $(menu_selector + " a").each(function () {
+            var hash = $(this).attr("href");
+            var target = $(hash);
+
+            if (target.position().top - 83 <= scroll_top && target.position().top - 83 + target.outerHeight() > scroll_top) {
+                $(menu_selector + " .menu__item_active").removeClass("menu__item_active");
+                $(this).parent().addClass("menu__item_active");
+            } else {
+                $(this).parent().removeClass("menu__item_active");
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        $(document).on("scroll", onScroll);
+        $('a[href^="#"]').click(function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+            $(menu_selector + " .menu__item_active").removeClass("menu__item_active");
+            $(this).parent().addClass("menu__item_active");
+            var hash = $(this).attr("href");
+            var target = $(hash);
+            $("html, body").animate({
+                scrollTop: target.offset().top - 83
+            }, 500, function () {
+                window.location.hash = hash;
+                $(document).on("scroll", onScroll);
+            });
+        });
+    });
+
 }
