33de988f739fe9f53cf14de9c08ea6c2abbb2b1b
Mikhail <mexcaptain@gmail.com>
2018-10-25 00:01:51 +0300

    JS and form fix

--- bit/static/js/main.js
+++ bit/static/js/main.js
@@ -1,7 +1,8 @@
 $(function () {
     slider();
     phoneMask();
-    scroll();
+    smoothScroll();
+    lightActiveMenu();
 });
 
 function slider() {
@@ -24,47 +25,42 @@ function slider() {
 }
 
 function phoneMask() {
-   var mask = new IMask($("[name='phone']")[0], {
-       mask: '+{7}(000) 000-00-00'
-   });
-   mask.rawValue = '999-12-12-123';
+    var mask = new IMask($("[name='phone']")[0], {
+        mask: '+{7}(000) 000-00-00'
+    });
+    mask.rawValue = '999-12-12-123';
 }
 
-function scroll() {
-    var menu_selector = ".js-menu";
-
-    function onScroll() {
-        var scroll_top = $(document).scrollTop();
-
-        $(menu_selector + " a").each(function () {
-            var hash = $(this).attr("href");
-            var target = $(hash);
-
-            if (target.position().top - 83 <= scroll_top && target.position().top - 83 + target.outerHeight() > scroll_top) {
-                $(menu_selector + " .menu__item_active").removeClass("menu__item_active");
-                $(this).parent().addClass("menu__item_active");
-            } else {
-                $(this).parent().removeClass("menu__item_active");
-            }
-        });
-    }
-
-    $(document).ready(function () {
-        $(document).on("scroll", onScroll);
-        $('a[href^="#"]').click(function (e) {
-            e.preventDefault();
-            $(document).off("scroll");
-            $(menu_selector + " .menu__item_active").removeClass("menu__item_active");
-            $(this).parent().addClass("menu__item_active");
-            var hash = $(this).attr("href");
-            var target = $(hash);
-            $("html, body").animate({
-                scrollTop: target.offset().top - 83
-            }, 500, function () {
-                window.location.hash = hash;
-                $(document).on("scroll", onScroll);
-            });
-        });
+function smoothScroll() {
+    $('a[href^="#"]').click(function (e) {
+        e.preventDefault();
+        var el = $(this).attr('href');
+        $('html,body').animate({
+            scrollTop: $(el).offset().top - $('.js-header').height()
+        }, 1000);
     });
+}
 
+function lightActiveMenu() {
+    $(window).on('scroll', function () {
+        var sections = $('section'),
+            lastId = sections.last().attr('id');
+        sections.each(function(i,el){
+            var top  = $(el).offset().top - $('.js-header').height(),
+                bottom = top +$(el).height(),
+                scroll = $(window).scrollTop(),
+                id = $(el).attr('id');
+
+            if (lastId === id) {
+                top -= 300;
+            }
+
+
+            if( scroll > top && scroll < bottom){
+                $('.js-menu .menu__item_active').removeClass('menu__item_active');
+                $('a[href="#'+id+'"]').parent().addClass('menu__item_active');
+            }
+        })
+
+    })
 }
